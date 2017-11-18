598c96fa8a5d9bbe8f32695cb6eff972045d56f6
himesama1301 <delta432h@gmail.com>
2017-11-19 03:13:59 +0900

    add water filter files.

---
+++ Source/Mizu_Assembly/Building_WaterFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MizuMod
+{
+    public class Building_WaterFilter : Building_WaterNet, IBuilding_WaterNet
+    {
+        public override bool IsSameConnector
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override void CreateConnectors()
+        {
+            this.InputConnectors.Clear();
+            this.OutputConnectors.Clear();
+
+            this.InputConnectors.Add(this.Position + this.Rotation.FacingCell * (-1));
+            this.OutputConnectors.Add(this.Position + this.Rotation.FacingCell);
+        }
+    }
+}
