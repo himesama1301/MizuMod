f95d18b259fdfe6d46b30d0a79605ec5da3cbf9a
himesama1301 <delta432h@gmail.com>
2017-11-22 22:07:13 +0900

    drawing recipe count all types of water items.

--- Source/Mizu_Assembly/MizuDef.cs
+++ Source/Mizu_Assembly/MizuDef.cs
@@ -10,27 +10,29 @@ namespace MizuMod
 {
     public static class MizuDef
     {
-        public static JobDef Job_DrinkWater = DefDatabase<JobDef>.GetNamed("Mizu_DrinkWater", true);
-        public static JobDef Job_FeedWaterPatient = DefDatabase<JobDef>.GetNamed("Mizu_FeedWaterPatient", true);
-        public static JobDef Job_DeliverWater = DefDatabase<JobDef>.GetNamed("Mizu_DeliverWater", true);
-        public static JobDef Job_DoBillFaucet = DefDatabase<JobDef>.GetNamed("Mizu_DoBillFaucet", true);
-        public static JobDef Job_DoBillTank = DefDatabase<JobDef>.GetNamed("Mizu_DoBillTank", true);
-        public static JobDef Job_DoBillWell = DefDatabase<JobDef>.GetNamed("Mizu_DoBillWell", true);
+        public static JobDef Job_DrinkWater = DefDatabase<JobDef>.GetNamed("Mizu_DrinkWater");
+        public static JobDef Job_FeedWaterPatient = DefDatabase<JobDef>.GetNamed("Mizu_FeedWaterPatient");
+        public static JobDef Job_DeliverWater = DefDatabase<JobDef>.GetNamed("Mizu_DeliverWater");
+        public static JobDef Job_DoBillFaucet = DefDatabase<JobDef>.GetNamed("Mizu_DoBillFaucet");
+        public static JobDef Job_DoBillTank = DefDatabase<JobDef>.GetNamed("Mizu_DoBillTank");
+        public static JobDef Job_DoBillWell = DefDatabase<JobDef>.GetNamed("Mizu_DoBillWell");
 
-        public static JobDef Job_DoBillDrawing = DefDatabase<JobDef>.GetNamed("Mizu_DoBillDrawing", true);
+        public static JobDef Job_DoBillDrawing = DefDatabase<JobDef>.GetNamed("Mizu_DoBillDrawing");
 
-        public static StatCategoryDef StatCategory_Water = DefDatabase<StatCategoryDef>.GetNamed("Mizu_WaterCategory", true);
+        public static StatCategoryDef StatCategory_Water = DefDatabase<StatCategoryDef>.GetNamed("Mizu_WaterCategory");
 
-        public static HediffDef Hediff_Dehydration = DefDatabase<HediffDef>.GetNamed("Mizu_Dehydration", true);
+        public static HediffDef Hediff_Dehydration = DefDatabase<HediffDef>.GetNamed("Mizu_Dehydration");
 
-        public static RecordDef Record_WaterDrank = DefDatabase<RecordDef>.GetNamed("Mizu_WaterDrank", true);
+        public static RecordDef Record_WaterDrank = DefDatabase<RecordDef>.GetNamed("Mizu_WaterDrank");
 
-        public static ThoughtDef Thought_DrankWaterDirectly = DefDatabase<ThoughtDef>.GetNamed("Mizu_DrankWaterDirectly", true);
+        public static ThoughtDef Thought_DrankWaterDirectly = DefDatabase<ThoughtDef>.GetNamed("Mizu_DrankWaterDirectly");
 
-        public static ThingDef Thing_ClearWater = DefDatabase<ThingDef>.GetNamed("Mizu_ClearWater", true);
-        public static ThingDef Thing_NormalWater = DefDatabase<ThingDef>.GetNamed("Mizu_NormalWater", true);
-        public static ThingDef Thing_RainWater = DefDatabase<ThingDef>.GetNamed("Mizu_RainWater", true);
-        public static ThingDef Thing_MudWater = DefDatabase<ThingDef>.GetNamed("Mizu_MudWater", true);
-        public static ThingDef Thing_SeaWater = DefDatabase<ThingDef>.GetNamed("Mizu_SeaWater", true);
+        public static ThingDef Thing_ClearWater = DefDatabase<ThingDef>.GetNamed("Mizu_ClearWater");
+        public static ThingDef Thing_NormalWater = DefDatabase<ThingDef>.GetNamed("Mizu_NormalWater");
+        public static ThingDef Thing_RainWater = DefDatabase<ThingDef>.GetNamed("Mizu_RainWater");
+        public static ThingDef Thing_MudWater = DefDatabase<ThingDef>.GetNamed("Mizu_MudWater");
+        public static ThingDef Thing_SeaWater = DefDatabase<ThingDef>.GetNamed("Mizu_SeaWater");
+
+        public static ThingCategoryDef ThingCategory_Waters = DefDatabase<ThingCategoryDef>.GetNamed("Mizu_Waters");
     }
 }
---
+++ Source/Mizu_Assembly/RecipeWorkerCounter_AnyWater.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace MizuMod
+{
+    public class RecipeWorkerCounter_AnyWater : RecipeWorkerCounter
+    {
+        private List<ThingDef> waterDefs;
+
+        public override bool CanCountProducts(Bill_Production bill)
+        {
+            return true;
+        }
+
+        public override int CountProducts(Bill_Production bill)
+        {
+            if (this.waterDefs == null)
+            {
+                // 水アイテム定義リストの生成(初回のみ)
+                this.waterDefs = new List<ThingDef>();
+                foreach (ThingDef current in DefDatabase<ThingDef>.AllDefsListForReading)
+                {
+                    if (current.thingCategories != null && current.thingCategories.Contains(MizuDef.ThingCategory_Waters))
+                    {
+                        this.waterDefs.Add(current);
+                    }
+                }
+            }
+            int num = 0;
+            for (int i = 0; i < this.waterDefs.Count; i++)
+            {
+                num += bill.Map.resourceCounter.GetCount(this.waterDefs[i]);
+            }
+            return num;
+        }
+
+        public override string ProductsDescription(Bill_Production bill)
+        {
+            return MizuDef.ThingCategory_Waters.label;
+        }
+    }
+}
