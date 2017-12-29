6544005367ef897aeedc583f52128ee2b3dfb054
himesama1301 <delta432h@gmail.com>
2017-12-29 10:57:14 +0900

    add comment.

--- Source/MizuMod/WorkGiver_DrawWater.cs
+++ Source/MizuMod/WorkGiver_DrawWater.cs
@@ -38,34 +38,44 @@ namespace MizuMod
             {
                 Bill bill = giver.BillStack[i];
 
+                // レシピが要求する仕事の種類と、WorkGiver側の仕事の種類があっているかチェック
                 if (bill.recipe.requiredGiverWorkType != null && bill.recipe.requiredGiverWorkType != this.def.workType) continue;
+
+                // 再チェック時間を過ぎていないかチェック(右クリックメニューからの場合は例外)
                 if (Find.TickManager.TicksGame < bill.lastIngredientSearchFailTicks + ReCheckFailedBillTicksRange.RandomInRange && FloatMenuMakerMap.makingFor != pawn) continue;
 
+                // チェック時間更新
                 bill.lastIngredientSearchFailTicks = 0;
 
+                // 今それをする必要があるか
                 if (!bill.ShouldDoNow()) continue;
+
+                // そのポーンが新規に仕事をできるか
                 if (!bill.PawnAllowedToStartAnew(pawn)) continue;
+
+                // レシピに必要なスキルを持っているか
                 if (!bill.recipe.PawnSatisfiesSkillRequirements(pawn))
                 {
                     JobFailReason.Is(MissingSkillTranslated);
                     continue;
                 }
 
+                // 水チェック
                 var getWaterRecipe = bill.recipe as GetWaterRecipeDef;
                 if (getWaterRecipe == null) return null;
 
+                // 必要な水の条件を満たしているか
                 var job = this.CreateJobIfSatisfiedWaterCondition(giver, getWaterRecipe, bill);
-                if (job != null)
-                {
-                    return job;
-                }
+                if (job != null)　return job;
 
                 if (FloatMenuMakerMap.makingFor != pawn)
                 {
+                    // 右クリックメニューからでなく、ジョブを開始できなかったらチェック時間を更新
                     bill.lastIngredientSearchFailTicks = Find.TickManager.TicksGame;
                 }
                 else
                 {
+                    // 右クリックメニューからの場合、できなかった理由を表示（素材不足）
                     JobFailReason.Is(MissingMaterialsTranslated);
                 }
             }
