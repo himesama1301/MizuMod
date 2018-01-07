8c07d356562976cd477739e97b2aff304e6f3b6b
himesama1301 <delta432h@gmail.com>
2018-01-07 13:40:28 +0900

    delete unused commented codes.

--- Source/MizuMod/JobGiver_GetWater.cs
+++ Source/MizuMod/JobGiver_GetWater.cs
@@ -52,33 +52,6 @@ namespace MizuMod
                 }
             }
 
-            //if (!pawn.CanDrinkFromTerrain())
-            //{
-            //    // 地形から直接水を摂取しない
-            //    //   →少しうろうろさせる
-            //    return null;
-            //}
-
-            // 前回使ったことがある水地形を探す
-            //int maxDistance = MaxDistanceOfSearchWaterTerrain;
-            //IntVec3 lastDirtyWaterVec = IntVec3.Invalid;
-            //if (need_water.lastDrinkTerrainPos != IntVec3.Invalid)
-            //{
-            //    // 地形変化で前回の場所が水地形でなくなっていないか確認
-            //    TerrainDef terrain = pawn.Map.terrainGrid.TerrainAt(need_water.lastDrinkTerrainPos);
-            //    if (terrain.CanGetWater())
-            //    {
-            //        // 前回の水地形までの距離を最大探索距離にする
-            //        maxDistance = (int)(need_water.lastDrinkTerrainPos - pawn.Position).LengthHorizontal;
-            //        lastDirtyWaterVec = need_water.lastDrinkTerrainPos;
-            //    }
-            //    else
-            //    {
-            //        // 前回の水地形が使えなくなっていたので情報をリセット
-            //        need_water.lastDrinkTerrainPos = IntVec3.Invalid;
-            //    }
-            //}
-
             // 何も見つからなかった場合は隠し水飲み場を探す
             // 人間、家畜、野生の動物全て
             IntVec3 hiddenWaterSpot;
@@ -90,68 +63,8 @@ namespace MizuMod
                 };
             }
 
-            // 地形利用処理が重いので削除、動物はどんなマップでも水が欲しい時には地形から摂取可能(家畜はダメ)
-            //   ただしアイテムを見つけた時は先にアイテムから飲む
-            //if (pawn.RaceProps.Animal && pawn.Faction != Faction.OfPlayer)
-            //{
-            //    need_water.CurLevel = 1.0f;
-            //}
-
-            // どの水地形を利用するか決める
-            //Predicate<IntVec3> validator = (vec) =>
-            //{
-            //    TerrainDef terrain = pawn.Map.terrainGrid.TerrainAt(vec);
-            //    return !vec.IsForbidden(pawn) && terrain.passability == Traversability.Standable && terrain.CanGetWater();
-            //};
-
-            //IntVec3 dirtyWaterVec = IntVec3.Invalid;
-            //bool isTerrainFound = CellFinder.TryRandomClosewalkCellNear(pawn.Position, pawn.Map, 300, out dirtyWaterVec, validator);
-
-            //if (isTerrainFound)
-            //{
-            //    // 水地形を発見
-            //    need_water.lastDrinkTerrainPos = dirtyWaterVec;
-            //    return new Job(MizuDef.Job_DrinkWater, dirtyWaterVec);
-            //}
-
-            //for (int i = 10; i < MaxDistanceOfSearchWaterTerrain;)
-            //{
-            //    // 近場は試行回数を増やす
-            //    int maxTrial = 1;
-            //    if (i < 50) maxTrial = 3;
-
-            //    for (int j = 0; j < maxTrial; j++)
-            //    {
-            //        IntVec3 dirtyWaterVec = IntVec3.Invalid;
-            //        bool isTerrainFound = CellFinder.TryRandomClosewalkCellNear(pawn.Position, pawn.Map, i, out dirtyWaterVec, validator);
-
-            //        if (isTerrainFound)
-            //        {
-            //            // 水地形を発見
-            //            need_water.lastDrinkTerrainPos = dirtyWaterVec;
-            //            return new Job(MizuDef.Job_DrinkWater, dirtyWaterVec);
-            //        }
-            //    }
-
-            //    if (i < 50)
-            //    {
-            //        i += 10;
-            //    }
-            //    else
-            //    {
-            //        i += 50;
-            //    }
-            //}
-
-            // 見つからなかったので前回のタイルを使用
-            //if (lastDirtyWaterVec != IntVec3.Invalid)
-            //{
-            //    return new Job(MizuDef.Job_DrinkWater, lastDirtyWaterVec);
-            //}
-
-            // 水を発見できず、前回の水地形情報もなし
+            // 水を発見できず
             return null;
-            //return new Job(JobDefOf.GotoWander);
         }
     }
 }
