605e759f48e7530f52d0ab1e6fdcc1bf3af42cf7
himesama1301 <delta432h@gmail.com>
2018-01-27 22:35:40 +0900

    re-fixed #118

--- Source/MizuMod/Mizu_Harmony.cs
+++ Source/MizuMod/Mizu_Harmony.cs
@@ -652,7 +652,7 @@ namespace MizuMod
                 List<CodeInstruction> new_codes = new List<CodeInstruction>();
                 new_codes.Add(new CodeInstruction(OpCodes.Ldloca_S, 11));
                 new_codes.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Rect), "get_x")));
-                new_codes.Add(new CodeInstruction(OpCodes.Ldc_R4, 100f));
+                new_codes.Add(new CodeInstruction(OpCodes.Ldc_R4, 400f));
                 new_codes.Add(new CodeInstruction(OpCodes.Add));
                 new_codes.Add(new CodeInstruction(OpCodes.Ldloca_S, 11));
                 new_codes.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Rect), "get_y")));
