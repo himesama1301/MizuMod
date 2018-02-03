a8dbf6221de66f108d7cb1f0d3e4179f6a54e18a
himesama1301 <delta432h@gmail.com>
2018-02-03 10:28:34 +0900

    fixed #161 (#165)
    * add minCategory to JobGiver_GetWater
    
    * fixed ThinkTree patch of pawns with non dehydration state.
    
    * add ThinkNode_ConditionalDehydration
    
    * add ConditionalDehydration node to ThinkTree

---
+++ Source/MizuMod/ThinkNode_ConditionalDehydration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+namespace MizuMod
+{
+    public class ThinkNode_ConditionalDehydration : ThinkNode_Conditional
+    {
+        protected override bool Satisfied(Pawn pawn)
+        {
+            return pawn.needs.water() != null && pawn.needs.water().CurCategory >= ThirstCategory.Dehydration;
+        }
+    }
+}
