7330cb910f7844cf6d00700f0f4fb8e28cc64236
himesama1301 <delta432h@gmail.com>
2018-02-03 18:15:36 +0900

    fixed #169

--- Source/MizuMod/RecipeWorkerCounter_DrawWater.cs
+++ Source/MizuMod/RecipeWorkerCounter_DrawWater.cs
@@ -20,17 +20,24 @@ namespace MizuMod
 
         public override bool CanCountProducts(Bill_Production bill)
         {
+            var ext = bill.recipe.GetModExtension<DefExtension_WaterRecipe>();
+            if (ext == null) return false;
+
+            var building = bill.billStack.billGiver as IBuilding_DrinkWater;
+            if (building == null) return false;
+
             return true;
         }
 
         public override int CountProducts(Bill_Production bill)
         {
-            int num = 0;
-            foreach (var waterDef in MizuDef.List_WaterItem)
-            {
-                num += bill.Map.resourceCounter.GetCount(waterDef);
-            }
-            return num;
+            var ext = bill.recipe.GetModExtension<DefExtension_WaterRecipe>();
+            var building = bill.billStack.billGiver as IBuilding_DrinkWater;
+
+            var waterDef = MizuUtility.GetWaterThingDefFromWaterType(building.WaterType);
+            int numOfWater = bill.Map.resourceCounter.GetCount(waterDef);
+
+            return numOfWater;
         }
 
         public override string ProductsDescription(Bill_Production bill)
