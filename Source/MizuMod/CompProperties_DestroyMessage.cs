857086dd9a3a6a2ad51d85c3912c74e3d915f827
himesama1301 <delta432h@gmail.com>
2018-02-18 21:26:43 +0900

    implemented #260 (#261)
    * added cost to snowman
    
    * added message in deconstructing snowman

---
+++ Source/MizuMod/CompProperties_DestroyMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Verse;
+
+namespace MizuMod
+{
+    public class CompProperties_DestroyMessage : CompProperties
+    {
+        public string messageKey;
+        public List<DestroyMode> destroyModes;
+
+        public CompProperties_DestroyMessage() : base(typeof(CompDestroyMessage)) { }
+        public CompProperties_DestroyMessage(Type compClass) : base(compClass) { }
+    }
+}
