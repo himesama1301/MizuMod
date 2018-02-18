ff0b35a460aae3e62660243ec7abc8971da301f7
himesama1301 <delta432h@gmail.com>
2018-02-18 14:06:05 +0900

    implemented #249

---
+++ Source/MizuMod/Area_SnowGet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Verse;
+
+namespace MizuMod
+{
+    public class Area_SnowGet : Area
+    {
+        public override string Label
+        {
+            get
+            {
+                return MizuStrings.AreaSnowGet.Translate();
+            }
+        }
+
+        public override Color Color
+        {
+            get
+            {
+                return new Color(0.5f, 0.0f, 0.0f);
+            }
+        }
+
+        public override int ListPriority
+        {
+            get
+            {
+                return 4000;
+            }
+        }
+
+        public Area_SnowGet()
+        {
+        }
+
+        public Area_SnowGet(AreaManager areaManager) : base(areaManager)
+		{
+        }
+
+        public override string GetUniqueLoadID()
+        {
+            return "Area_" + this.ID + "_MizuSnowGet";
+        }
+    }
+}
