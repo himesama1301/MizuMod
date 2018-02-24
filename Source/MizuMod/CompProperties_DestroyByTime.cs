917131c854f3ce0b2e68bdc4295b880ed331dde5
himesama1301 <delta432h@gmail.com>
2018-02-24 20:19:19 +0900

    implemented #290

---
+++ Source/MizuMod/CompProperties_DestroyByTime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace MizuMod
+{
+    public class CompProperties_DestroyByTime : CompProperties
+    {
+        public int destroyTicks = 1;
+
+        public CompProperties_DestroyByTime() : base(typeof(CompDestroyByTime)) { }
+        public CompProperties_DestroyByTime(Type compClass) : base(compClass) { }
+    }
+}
