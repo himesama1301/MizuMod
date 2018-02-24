1793387d8bf1ca1eb0bd4c46441fbedf06e9003f
himesama1301 <delta432h@gmail.com>
2018-02-24 11:13:30 +0900

    Issue286maste (#287)
    * added mop area
    
    * added workgiver and job for mop
    
    * added jobdriver for mop

---
+++ Source/MizuMod/Designator_AreaMopExpand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace MizuMod
+{
+    public class Designator_AreaMopExpand : Designator_AreaMop
+    {
+        public Designator_AreaMopExpand() : base(DesignateMode.Add)
+        {
+            this.defaultLabel = MizuStrings.DesignatorAreaMopExpand.Translate();
+            this.defaultDesc = MizuStrings.DesignatorAreaMopExpandDescription.Translate();
+            this.icon = ContentFinder<Texture2D>.Get("UI/Designators/HomeAreaOn", true);
+            this.soundDragSustain = SoundDefOf.DesignateDragAreaAdd;
+            this.soundDragChanged = SoundDefOf.DesignateDragAreaAddChanged;
+            this.soundSucceeded = SoundDefOf.DesignateAreaAdd;
+        }
+    }
+}
