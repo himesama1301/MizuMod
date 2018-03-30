bae940814ab1920739c360da37a08b850c76ff16
himesama1301 <delta432h@gmail.com>
2018-03-30 20:24:28 +0900

    added empty mod settings

---
+++ Source/MizuMod/MizuModBody.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace MizuMod
+{
+    public class MizuModBody : Mod
+    {
+        public MizuModBody(ModContentPack content) : base(content)
+        {
+
+        }
+
+        public override string SettingsCategory()
+        {
+            return "No Water, No Life.";
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+        }
+    }
+}
