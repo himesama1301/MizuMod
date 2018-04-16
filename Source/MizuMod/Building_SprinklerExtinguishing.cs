7cbe3afc915abb0bcc7aee8c88ed786141bb209f
himesama1301 <delta432h@gmail.com>
2018-04-16 21:46:21 +0900

    added basic waternet settings

---
+++ Source/MizuMod/Building_SprinklerExtinguishing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Verse;
+using RimWorld;
+
+namespace MizuMod
+{
+    public class Building_SprinklerExtinguishing : Building_WaterNet, IBuilding_WaterNet
+    {
+        private const int ExtinguishPower = 50;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+        }
+
+        public override void TickRare()
+        {
+            base.TickRare();
+        }
+    }
+}
