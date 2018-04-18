ebd7512c93172e5ea93fee10347f83727d9a94ce
himesama1301 <delta432h@gmail.com>
2018-04-18 22:29:57 +0900

    added empty verb class

---
+++ Source/MizuMod/Verb_LaunchBucketWater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Verse;
+
+namespace MizuMod
+{
+    public class Verb_LaunchBucketWater : Verb_LaunchProjectile
+    {
+        public override void WarmupComplete()
+        {
+            Log.Message("WarmupComplete");
+            base.WarmupComplete();
+        }
+
+        protected override bool TryCastShot()
+        {
+            Log.Message("TryCastShot");
+            return base.TryCastShot();
+        }
+    }
+}
